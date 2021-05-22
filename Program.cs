fd7c4f5f42a557931d10c14b48826fb750614bb5
Andrea <Andreabpr37@gmail.com>
2021-05-22 03:10:04 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+namespace ejercicio_basicos_DDR_1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            String nombre = "Andrea";
+
+            String ciudad = "Valladolid";
+
+            Console.WriteLine("Hola" + nombre + " bienvenida a" + ciudad);
+
+        }
+    }
+}
